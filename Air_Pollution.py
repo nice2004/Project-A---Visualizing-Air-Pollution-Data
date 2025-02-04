758c2df8f40560bd37dd75ebc2adcc81d95dfee2
nice <nhirwa@westmont.edu>
2025-02-03 21:47:01 -0800

    Added in the structure of the project

---
+++ Air_Pollution.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import plotly.express as px
+from dash import Dash, doc, html, Input, Output
+
+# Plotly graphs aren't just static images.
+# Users can hover over data points to see details,
+# zoom in and out, pan around, and even interact with
+# elements in the graph to explore the data
+
+# Preparing my data for usage *********************************************************
+
+# App Layout **************************************************************************
+
+# Callbacks ***************************************************************************
