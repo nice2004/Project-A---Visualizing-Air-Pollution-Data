9354cf884e156cf0675225a9cd33a5ac2d2d613c
nice <nhirwa@westmont.edu>
2025-02-10 17:36:17 -0800

    Finished writing my README

--- Air_Pollution.py
+++ Air_Pollution.py
@@ -1,14 +1,80 @@
 import pandas as pd
 import plotly.express as px
-from dash import Dash, doc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output
 
-# Plotly graphs aren't just static images.
-# Users can hover over data points to see details,
-# zoom in and out, pan around, and even interact with
-# elements in the graph to explore the data
+# Plotly graphs aren't just static images. Users can hover over data points to see details,
+# zoom in and out, pan around, and even interact with elements in the graph to explore the data
 
-# Preparing my data for usage *********************************************************
+
+# Exploring my data & Preparing my data for usage *********************************************************
+df = pd.read_csv('San_Franscisco_Dataset.csv')
+print(df.head())
+print(df['Daily Max 8-hour CO Concentration'])
+
+has_missing_values1 = df['Daily Max 8-hour CO Concentration'].isnull().any()
+has_missing_values2 = df['Daily AQI Value'].isnull().any()
+has_missing_values3 = df['Local Site Name'].isnull().any()
+has_missing_values4 = df['Date'].isnull().any()
+print(has_missing_values1)
+print(has_missing_values1)
+print(has_missing_values1)
+print(has_missing_values1)
+
+# changing the date column to date time
+df['Date'] = pd.to_datetime(df['Date'])
 
 # App Layout **************************************************************************
+# this goes to styles.css and tells it what to do!
+stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+app = Dash(__name__, external_stylesheets=stylesheets)
+
+# dcc provides a wide range of components like dropdowns,
+# sliders, input boxes, graphs, date pickers, and more, allowing users to interact with your app
+app.layout = html.Div([
+    dcc.Dropdown(id='site-dropdown',
+                 options=[{'label': site, 'value': site} for site in df['Local Site Name'].unique()],
+                 multi=True,
+                 value=df['Local Site Name'].unique()[0]  # default to one site
+                 ), dcc.Graph(id='Air Index Graph in San Francisco')
+])
+
 
 # Callbacks ***************************************************************************
+
+# callbacks are used to make the dashboard interactive.
+# They allow you to define how the dashboard should respond
+# to user interactions like clicking a button, selecting a value from a dropdown,
+# or dragging a slider.
+
+@app.callback(Output('Air Index Graph in San Francisco', 'figure'),
+              Input('site-dropdown', 'value'))
+# is in is always used for sites only, so created a list for local sites
+def update_graph(selected_site):
+    if isinstance(selected_site, str):
+        selected_site = [selected_site]
+    # filter the dataframe to include only the selected sites
+    filtered_df = df[df['Local Site Name'].isin(selected_site)]
+    fig = px.bar(filtered_df, x='Date',
+                 y='Daily AQI Value', color='Local Site Name', title=f'Air Quality Index for {selected_site} over Time',
+                 labels={'Date': "Date", 'Daily AQI Value': 'AQI Value'})
+    fig.update_layout(
+        title={
+            'text': f'Air Quality Index for {selected_site} over Time',
+            'y': 0.9,  # vertical position
+            'x': 0.5,  # Center the Title
+            'xanchor': 'center',
+            'yanchor': 'top',
+            'font': {
+                'size': 25,
+                'family': 'Arial, sans-serif',
+                'color': 'black',
+                'weight': 'bold'
+
+            }
+        }
+    )
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Project-A---Visualizing-Air-Pollution-Data
+
+# Names: Nice Teta Hirwa
+# Instructor: Professor Mike Ryu
+# Class: CS-150
+
+# Thesis Statement
+Exploring the relationship between the air quality index in North California local sites and the months in the year of 2020
+
+# Context of my data visualization 
+In the year of 2020, industries, gast stations and other areas that contribute to air pollution was almost closed.
+But did the air quality index become better? Through my project, I try to answer this question by navigating the relationship between the
+air pollution index and how it how was distributed over the months in 2020. I use the North California dataset that displays months, different local 
+sites in North California and the air index quality variables. I then plotted these variables together in a dashboard to portray the thesis of the project.
+
+# Data I will be visualizing
+I will visualize air quality index of different local sites in North California from January to December 2020
+
+# Strategies employed from SWD
+1.  Articulating my unique point of view of the project
+2. Specifically conveying what’s at stake
+3. Displaying what is happening, what should be the audiences response, and how is the data being displayed correctly
+
+# Explaining the coding part of the project
+Air_Pollution.py has three main parts that makes the visual display effectively. 
+1. Preparing the data: Here, I printed out the top of the csv to see how it looks like, I checked if there is any missing numbers in any of teh columns that I wanted to use
+and I finally changed the date into date time. 
+2. App_Layout: In the app layout, I created my dashboard with drop-downs that would allow multiple comparison of different 
+local sites, and created an interface where users can interact with the dashboard by adding or removing some local sites.
+3. Call back: In my call back function, I have a function called updated_graph that updates the graph and layout(like color and keys) everytime a user adds in another local site to compare. It also
+filters the dataframe to include only the selected sites with its title. 
+
+And I finally called the main() function to run the code.
+
